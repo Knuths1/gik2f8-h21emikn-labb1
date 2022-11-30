789704a26d531ed260ee045d588e8629279ea1e1
Knuths1 <h21emikn@du.se>
2022-11-30 13:35:03 +0100

    Tagit bort överflödig kod

--- Components/BookInfo.js
+++ Components/BookInfo.js
@@ -1,6 +1,5 @@
 const BookInfo = (book) => {
-    //console.log(book)
-    let html = `
+  let html = `
               <div class="book-info -ml-80">
                 <div class="rounded-md border-2 border-black bg-gray-400 w-full mx-auto w-60">
                  <div class="p-3">
@@ -17,33 +16,5 @@ const BookInfo = (book) => {
                   </div>
                   </div>
               </div>`;
-    return html;
-
-
-    // const bild = book.coverImage
-    // const title = book.title
-    // const author = book.author
-    // const release = book.release
-
-    // let html = `
-    //             <div class="rounded-md border-2 border-blue-400 bg-white w-full mx-auto w-40">
-    //             <div class="">
-    //               <div class="">
-    //                 <div class="">
-    //                   <img src=    "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstephenking.com%2Fpromo%2Fdark-tower-2016-collection%2Fimages%2Fthe_gunslinger.jpg"/>
-    //                   <div class="">
-    //                     <span class="">Author: ${book.title} </span>
-    //                     <span class="">Title: ${book.author}</span>
-    //                     <span class="">Pages: ${book.pages}</span>
-    //                     <span class="">Release Date: ${book.releaseDate}</span>
-    //                   </div> 
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           </div>`;
-    // return html;
-    //console.log(book)
-
-    // let html = `<h1>TEST</h1>`
-    // return html
-  };
+  return html;
+};
--- script.js
+++ script.js
@@ -1,73 +1,56 @@
-'use strict';
+"use strict";
 
 let bookList = [];
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   getAll().then((apiBooks) => (bookList = apiBooks));
 });
 
-
-searchField.addEventListener('keyup', (e) =>
+searchField.addEventListener("keyup", (e) =>
   renderBookList(
     bookList.filter(({ title, author }) => {
       const searchTerm = e.target.value.toLowerCase();
       return (
         title.toLowerCase().includes(searchTerm.toLowerCase()) ||
         author.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-      })
-      )
       );
-      
-function renderBookList(bookList) {
-  const existingElement = document.querySelector('.book-list');
-  const root = document.getElementById('root');
-  existingElement && root.removeChild(existingElement);
-  bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
-  
- 
-  var elements = document.getElementsByClassName('point');
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('mouseover', (e) => {
-      let myBook = getOne(e.target.id)
-      myBook.then(function(result) {
-        renderBookItem(result);
-      })
     })
-    elements[i].addEventListener('mouseout', (e) => {
-      const insert = document.getElementById('bookDetail');
-      const existingElement = document.querySelector('.book-info');
+  )
+);
+
+function renderBookList(bookList) {
+  const existingElement = document.querySelector(".book-list");
+  const root = document.getElementById("root");
+  existingElement && root.removeChild(existingElement);
+  bookList.length > 0 &&
+    searchField.value &&
+    root.insertAdjacentHTML("beforeend", BookList(bookList));
+
+  var elements = document.getElementsByClassName("point");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("mouseover", (e) => {
+      const rutan = `<div id="bookDetail" class="position absolute"></div>`;
+      root.insertAdjacentHTML("beforeend", rutan);
+
+      let myBook = getOne(e.target.id);
+      myBook.then(function (result) {
+        renderBookItem(result);
+      });
+    });
+    elements[i].addEventListener("mouseout", (e) => {
+      const insert = document.getElementById("bookDetail");
+      const existingElement = document.querySelector(".book-info");
       existingElement && insert.removeChild(existingElement);
-      })
-
+    });
   }
-  
-
-
-
-//   let cursor = document.querySelector('.point')
-//   cursor.addEventListener('mouseover', (e) => {
-//     let myBook = getOne(e.target.id)
-//     myBook.then(function(result) {
-//     renderBookItem(result);
-//   })   
-// })
-
-
-
 }
-  
 
-
-function renderBookItem(book){
-  //console.log(book)
-  
-  const existingElement = document.querySelector('.book-info');
-  const insert = document.getElementById('bookDetail');
+function renderBookItem(book) {
+  //const rutan = `<div id="bookDetail" class="position absolute"></div>`;
+  //root.insertAdjacentHTML("beforeend", rutan);
+  const existingElement = document.querySelector(".book-info");
+  const insert = document.getElementById("bookDetail");
   existingElement && insert.removeChild(existingElement);
-  let minhtml = BookInfo(book)
-  insert.insertAdjacentHTML('afterbegin', minhtml)
+  let minhtml = BookInfo(book);
+  insert.insertAdjacentHTML("afterbegin", minhtml);
 }
-
-const rutan = `<div id="bookDetail" class="position absolute"></div>`;
-root.insertAdjacentHTML('beforeend',rutan)
